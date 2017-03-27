377849a3d0462c72074a669a1c3fa0d10ef9e62e
raniel <raniel.one@gmail.com>
2017-03-27 13:51:56 +0800

    added seed for instantiating user credentials. / added some instruction on README.md

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name:"admin", password:"admin",admin:"1")
+User.create(name:"student",password:"student",admin:"0")
